bcb351b9df43a9c65bead2649fde46ac46241d7d
Max Vigdorchik <mdv29@pip.srcf.societies.cam.ac.uk>
2019-01-20 06:09:39 +0000

    Bring everything together and have AI actually make decisions

--- callbot.py
+++ callbot.py
@@ -6,7 +6,7 @@ import numpy as np
 import pymc3 as pm
 import logging
 logger = logging.getLogger("pymc3")
-logger.propagate = False
+logger.setLevel(logging.ERROR)
 
 simulation_num = 1000
 
@@ -21,25 +21,46 @@ class CallBot(BasePokerPlayer):
             hole_card=gen_cards(hole_card),
             community_card=gen_cards(community)
         )
-        print("Win Rate Is ", win_rate)
 
         with action_model:
             win_chance = pm.Normal('win_chance', mu=win_rate, sd=np.sqrt(
                 win_rate*(1-win_rate)/simulation_num))
             # TODO: Standard deviations for confidence can be calculated in a smarter way
-            confidences = [
-                pm.Normal('confidence_' + id, mu=self.round_players[id]['confidence'][0], sd=0.2) for id in self.game_uuids]
             mu = pm.Normal('base', mu=1, sd=0.3)
-            bluffs = [pm.Normal('bluff_' + id, mu=mu, sd=0.3,
-                                observed=self.game_players[id]['bluffs']) for id in self.game_uuids]
+            for id in self.game_uuids:
+                confidence = pm.Normal(
+                    'confidence_' + id, mu=self.round_players[id]['confidence'][0], sd=0.2)
+                bluff = pm.Normal('bluff_' + id, mu=mu, sd=0.3,
+                                  observed=self.game_players[id]['bluffs'])
 
             trace = pm.sample(500, progressbar=False)
             post_pred = pm.sample_posterior_predictive(
-                trace, samples=500, progressbar=False)
-            print(np.mean(post_pred['bluff_'+self.game_uuids[0]]))
+                trace, samples=1000, progressbar=False)
+            bets = []
+            for id in self.game_uuids:
+                bet = trace['win_chance'] - trace['confidence_'+id] / \
+                    self.game_players[id]['average_confidence'][0] * \
+                    np.tanh(1-10*post_pred['bluff_'+id][:, 0])
+                bets.append(np.mean(bet))
+            print("potential bets are ", bets)
 
-        actions = [item for item in valid_actions if item['action'] in ['call']]
-        return list(np.random.choice(actions).values())
+        target_bet = np.min(bets)
+        pot = round_state['pot']['main']['amount']
+
+        call = [item for item in valid_actions if item['action'] in ['call']]
+        p_raise = [item for item in valid_actions if item['action'] in ['raise']]
+        if len(call) > 0 and call[0]['amount'] > target_bet*pot*1.1:
+            return 'fold', 0
+        elif len(p_raise) > 0 and len(call) > 0 and target_bet*pot > call[0]['amount'] * 1.1:
+            if target_bet*pot > p_raise[0]['amount']['min']:
+                if target_bet*pot < p_raise[0]['amount']['max']:
+                    return 'raise', target_bet*pot
+                else:
+                    return 'raise', p_raise[0]['amount']['max']
+        else:
+            return call[0].values()
+
+        return 'fold', 0
 
     def receive_game_start_message(self, game_info):
         self.nb_player = game_info['player_num']
@@ -85,7 +106,11 @@ class CallBot(BasePokerPlayer):
                 if action[0] == 'fold':
                     confidence = 0
                     break
-                confidence += action[1]/action[2]  # Amount divided by pot size
+                if action[0] == 'raise':
+                    confidence += action[1]/action[2] * 1.2
+                else:
+                    # Amount divided by pot size
+                    confidence += action[1]/action[2]
             # Keep track of action number as well
             self.round_players[uuid]['confidence'] = (
                 confidence, len(self.round_players[uuid]['actions']))
@@ -100,17 +125,16 @@ class CallBot(BasePokerPlayer):
                     total + self.round_players[uuid]['confidence'][0], number + 1)
 
                 if uuid in win_ids:
-                    total, number = self.game_players[uuid]['average_win_confidence']
+                    total_win, number_win = self.game_players[uuid]['average_win_confidence']
                     self.game_players[uuid]['average_win_confidence'] = (
-                        total + self.round_players[uuid]['confidence'][0], number + 1)
+                        total_win + self.round_players[uuid]['confidence'][0], number_win + 1)
                 else:
-                    total, number = self.game_players[uuid]['average_loss_confidence']
+                    total_lose, number_lose = self.game_players[uuid]['average_loss_confidence']
                     self.game_players[uuid]['average_loss_confidence'] = (
-                        total + self.round_players[uuid]['confidence'][0], number + 1)
+                        total_lose + self.round_players[uuid]['confidence'][0], number_lose + 1)
 
                     self.game_players[uuid]['bluffs'].append(
                         self.round_players[uuid]['confidence'][0]/(total / number))
-        print(self.game_players[self.game_uuids[0]]['bluffs'])
         self.save_data()
 
     def save_data(self):
