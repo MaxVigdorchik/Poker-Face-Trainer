4edd96fe4d8e4b294d0d3c505d1be3d5c9d564af
Edwin Balani <git@balani.xyz>
2019-01-20 11:27:29 +0000

    Add working emotion analysis capture code

--- Emotion_Analysis.py
+++ Emotion_Analysis.py
@@ -1,8 +1,14 @@
-def get_emotion_data(self):
-    rgb = self.vids.get_frame()
-    jpg = cv2.imencode('jpg', rgb)
+import cv2
+import requests
+
+from .video_capture import MyVideoCapture
+vids = MyVideoCapture()
+
+def get_emotion_data():
+    ret, rgb = vids.get_frame()
+    result, jpg = cv2.imencode('.jpg', rgb)
     subscription_key = '1a58d3cda9554726b64dae6aba761774'
-    assert subscription_key
+    jpg = jpg.tostring()
 
     emotion_recognition_url = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=emotion"
 
@@ -13,3 +19,5 @@ def get_emotion_data(self):
     response.raise_for_status()
     analysis = response.json()
     print(analysis)
+
+get_emotion_data()
